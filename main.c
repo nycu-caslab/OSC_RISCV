8c077f759483489cc32b2982aebe1c1fa31c087f
Allen Guan <me@nella17.tw>
2024-08-16 01:11:56 +0800

    GPIO reset

--- main.c
+++ main.c
@@ -74,8 +74,49 @@ int readline(char *buffer, int length) {
   return r;
 }
 
+// ref:
+// https://github.com/starfive-tech/opensbi/blob/VF2_6.1_v3.8.2/lib/utils/gpio/fdt_gpio_starfive.c#L55-L74
+
+#define STARFIVE_GPIO_OUTVAL 0x40
+#define STARFIVE_GPIO_MASK 0xff
+#define STARFIVE_GPIO_REG_SHIFT_MASK 0x3
+#define STARFIVE_GPIO_SHIFT_BITS 0x3
+
+typedef unsigned int u32;
+
+static inline void writel(u32 val, volatile void *addr) {
+  asm volatile("sw %0, 0(%1)" : : "r"(val), "r"(addr));
+}
+static inline u32 readl(const volatile void *addr) {
+  u32 val;
+  asm volatile("lw %0, 0(%1)" : "=r"(val) : "r"(addr));
+  return val;
+}
+
+void starfive_gpio_set(long addr, long offset, int value) {
+  unsigned long reg_addr = addr + offset;
+  reg_addr &= ~(STARFIVE_GPIO_REG_SHIFT_MASK);
+
+  u32 shift_bits = (offset & STARFIVE_GPIO_REG_SHIFT_MASK)
+                   << STARFIVE_GPIO_SHIFT_BITS;
+  u32 bit_mask = STARFIVE_GPIO_MASK << shift_bits;
+  /* set output value */
+  u32 val = readl((void *)(reg_addr + STARFIVE_GPIO_OUTVAL));
+  val &= ~bit_mask;
+  val |= value << shift_bits;
+  writel(val, (void *)(reg_addr + STARFIVE_GPIO_OUTVAL));
+}
+
 void reset() {
-  // TODO
+  puts("resetting ... \n");
+
+  const int offset = 35;
+  long addr = 0x13040000;
+  starfive_gpio_set(addr, offset, 1);
+
+  for (;;) {
+    putchar('.');
+  }
 }
 
 void kernel_main() {
