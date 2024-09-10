32dddea6961ec0569a841473037e7aeed99de097
Allen Guan <me@nella17.tw>
2024-09-10 13:34:26 +0800

    copy bootloader

--- Makefile
+++ Makefile
@@ -2,33 +2,43 @@ QEMU 		:= qemu-system-riscv64
 MINICOM 	:= minicom
 SERIAL 		:= /dev/cu.usbserial-0001
 
-OUTPUT_DIR 	:= $(PWD)/build
-export OUTPUT_DIR
-SRC_DIR  	:= $(PWD)/src
+KERNEL_SRC  := $(PWD)/src
+BOOT_SRC  	:= $(PWD)/bootloader
 DISK_DIR 	:= $(PWD)/disk
 
 KERNEL_BIN 	:= $(DISK_DIR)/kernel.img
+BOOTLOADER 	:= $(DISK_DIR)/bootloader.img
 
 QEMUFLAGS  	:=
 QEMUFLAGS 	+= -M virt
 QEMUFLAGS 	+= -display none
 QEMUFLAGS 	+= -serial stdio
 QEMUFLAGS 	+= -smp cpus=4
+QEMUFLAGS 	+= -m 4G
 
-.PHONY: all clean build qemu uart
+.PHONY: all clean kernel bootloader build qemu uart
 all: build qemu
 
-build: $(KERNEL_BIN)
+build: kernel
 
+kernel: $(KERNEL_BIN)
 $(KERNEL_BIN):
-	${MAKE} -C $(SRC_DIR) TARGET_BIN=$(KERNEL_BIN)
+	${MAKE} -C $(KERNEL_SRC) TARGET_BIN=$(KERNEL_BIN)
+
+bootloader: $(BOOTLOADER)
+$(BOOTLOADER):
+	${MAKE} -C $(BOOT_SRC) TARGET_BIN=$(BOOTLOADER)
 
 clean:
-	${MAKE} -C $(SRC_DIR) TARGET_BIN=$(KERNEL_BIN) clean
+	${MAKE} -C $(KERNEL_SRC) TARGET_BIN=$(KERNEL_BIN) clean
+	${MAKE} -C $(BOOT_SRC) TARGET_BIN=$(BOOTLOADER) clean
 	$(RM) $(KERNEL_BIN)
 
 qemu: $(KERNEL_BIN)
 	$(QEMU) $(QEMUFLAGS) $(QEMU_EXT_FLAGS) -kernel $(KERNEL_BIN)
+
+qemu-bootloader: $(BOOTLOADER)
+	$(QEMU) $(QEMUFLAGS) $(QEMU_EXT_FLAGS) -kernel $(BOOTLOADER)
 
 uart:
 	$(MINICOM) -D $(SERIAL)
---
+++ bootloader/main.c
@@ -0,0 +1,162 @@
+#define NOP asm volatile("ADDI x0, x0, 0")
+
+#define UART_BASE 0x10000000LL
+#define UART_THR_OFFSET 0
+#define UART_RBR_OFFSET 0
+#define UART_LSR_OFFSET 5
+
+int UART_UNIT_SIZE = 4;
+
+unsigned int UART_GET(int offset) {
+  switch (UART_UNIT_SIZE) {
+  case 1:
+    return *(unsigned char *)(UART_BASE + offset * UART_UNIT_SIZE);
+  case 4:
+    return *(unsigned int *)(UART_BASE + offset * UART_UNIT_SIZE);
+  default:
+    __builtin_unreachable();
+  }
+}
+
+void UART_SET(int offset, unsigned int value) {
+  switch (UART_UNIT_SIZE) {
+  case 1:
+    *(unsigned char *)(UART_BASE + offset * UART_UNIT_SIZE) = value;
+    return;
+  case 4:
+    *(unsigned int *)(UART_BASE + offset * UART_UNIT_SIZE) = value;
+    return;
+  default:
+    __builtin_unreachable();
+  }
+}
+
+void uart_write(char c) {
+  while ((UART_GET(UART_LSR_OFFSET) & ((1 << 5) | (1 << 6))) == 0)
+    NOP;
+  UART_SET(UART_THR_OFFSET, c);
+}
+
+char uart_read() {
+  while ((UART_GET(UART_LSR_OFFSET) & (1 << 0)) == 0)
+    NOP;
+  return UART_GET(UART_RBR_OFFSET);
+}
+
+int putchar(int c) {
+  if (c == '\n')
+    uart_write('\r');
+  uart_write(c);
+  return 1;
+}
+
+int getchar() {
+  char c = uart_read();
+  return c == '\r' ? '\n' : c;
+}
+
+void puts(const char *s) {
+  while (*s != 0)
+    putchar(*s++);
+}
+
+int readline(char *buffer, int length) {
+  int r = 0;
+  while (r < length) {
+    char c = getchar();
+    putchar(c);
+    if (c == '\n') {
+      buffer[r++] = 0;
+      break;
+    }
+    buffer[r++] = c;
+  }
+  return r;
+}
+
+// ref:
+// https://github.com/starfive-tech/opensbi/blob/VF2_6.1_v3.8.2/lib/utils/gpio/fdt_gpio_starfive.c#L55-L74
+
+#define STARFIVE_GPIO_OUTVAL 0x40
+#define STARFIVE_GPIO_MASK 0xff
+#define STARFIVE_GPIO_REG_SHIFT_MASK 0x3
+#define STARFIVE_GPIO_SHIFT_BITS 0x3
+
+typedef unsigned int u32;
+
+static inline void writel(u32 val, volatile void *addr) {
+  asm volatile("sw %0, 0(%1)" : : "r"(val), "r"(addr));
+}
+static inline u32 readl(const volatile void *addr) {
+  u32 val;
+  asm volatile("lw %0, 0(%1)" : "=r"(val) : "r"(addr));
+  return val;
+}
+
+void starfive_gpio_set(long addr, long offset, int value) {
+  unsigned long reg_addr = addr + offset;
+  reg_addr &= ~(STARFIVE_GPIO_REG_SHIFT_MASK);
+
+  u32 shift_bits = (offset & STARFIVE_GPIO_REG_SHIFT_MASK)
+                   << STARFIVE_GPIO_SHIFT_BITS;
+  u32 bit_mask = STARFIVE_GPIO_MASK << shift_bits;
+  /* set output value */
+  u32 val = readl((void *)(reg_addr + STARFIVE_GPIO_OUTVAL));
+  val &= ~bit_mask;
+  val |= value << shift_bits;
+  writel(val, (void *)(reg_addr + STARFIVE_GPIO_OUTVAL));
+}
+
+void reset() {
+  puts("resetting ... \n");
+
+  const int offset = 35;
+  long addr = 0x13040000;
+  starfive_gpio_set(addr, offset, 1);
+
+  for (;;) {
+    putchar('.');
+  }
+}
+
+void kernel_main() {
+  /*
+// FIXME
+const int cnt = 10;
+for (int i = 0; i <= cnt; i++) {
+  if ((UART_GET(UART_LSR_OFFSET) & ((1 << 5) | (1 << 6))) != 0)
+    break;
+  if (i == cnt)
+    UART_UNIT_SIZE = 4;
+}
+*/
+
+  puts("Hello Kernel!\n");
+
+#define BUF_SIZE 1024
+  char buf[BUF_SIZE];
+
+  for (;;) {
+    puts("$ ");
+    int len = readline(buf, BUF_SIZE);
+    if (len <= 0)
+      continue;
+    switch (buf[0]) {
+    case 'h':
+      switch (buf[3]) {
+      case 'p':
+        puts("help\t: print this help menu\n");
+        puts("hello\t: print Hello World!\n");
+        puts("reboot\t: reboot the device\n");
+        break;
+      case 'l':
+        puts("Hello World!\n");
+        break;
+      }
+      break;
+    case 'r':
+      reset();
+      break;
+    }
+  }
+}
