590d86d7d542aff95eb027c797c103cf1f173e27
Allen Guan <me@nella17.tw>
2024-09-10 13:38:24 +0800

    bootloader

--- bootloader/main.c
+++ bootloader/main.c
@@ -5,7 +5,8 @@
 #define UART_RBR_OFFSET 0
 #define UART_LSR_OFFSET 5
 
-int UART_UNIT_SIZE = 4;
+// int UART_UNIT_SIZE = 1;
+#define UART_UNIT_SIZE 4
 
 unsigned int UART_GET(int offset) {
   switch (UART_UNIT_SIZE) {
@@ -50,28 +51,16 @@ int putchar(int c) {
   return 1;
 }
 
+char getchar_raw() { return uart_read(); }
+
 int getchar() {
-  char c = uart_read();
+  char c = getchar_raw();
   return c == '\r' ? '\n' : c;
 }
 
 void puts(const char *s) {
   while (*s != 0)
     putchar(*s++);
-}
-
-int readline(char *buffer, int length) {
-  int r = 0;
-  while (r < length) {
-    char c = getchar();
-    putchar(c);
-    if (c == '\n') {
-      buffer[r++] = 0;
-      break;
-    }
-    buffer[r++] = c;
-  }
-  return r;
 }
 
 // ref:
@@ -119,44 +108,142 @@ void reset() {
   }
 }
 
-void kernel_main() {
-  /*
-// FIXME
-const int cnt = 10;
-for (int i = 0; i <= cnt; i++) {
-  if ((UART_GET(UART_LSR_OFFSET) & ((1 << 5) | (1 << 6))) != 0)
-    break;
-  if (i == cnt)
-    UART_UNIT_SIZE = 4;
+char i2h(int x) {
+  if (0 <= x && x <= 9)
+    return '0' + x;
+  if (10 <= x && x <= 15)
+    return 'A' + x - 10;
+  return '?';
 }
-*/
 
-  puts("Hello Kernel!\n");
+void *memcpy(void *dst, const void *src, long n) {
+  char *d = (char *)dst;
+  const char *s = (const char *)src;
+  for (long i = 0; i < n; i++) {
+    *d = *s;
+    if (*d != *s) {
+      puts("memcpy fail !!???\n");
+      while (1)
+        ;
+    }
+    d++;
+    s++;
+  }
+  return dst;
+}
+
+void print_hex(long value) {
+  long x = 1;
+  while (value / x >= 16)
+    x *= 16;
+  puts("0x");
+  for (; x; x /= 16)
+    putchar(i2h(value / x % 16));
+}
+
+void read(void *p, long size) {
+  char *s = p;
+  for (long i = 0; i < size; i++) {
+    char c = getchar_raw();
+    s[i] = c;
+  }
+}
+
+void hexdump(const void *buf, unsigned size, const char *msg) {
+  const char *s = (const char *)buf;
+  puts(msg ? msg : __func__);
+  puts(": ");
+  print_hex(size);
+  puts(" bytes dumped\n");
+  for (unsigned i = 0; i < size; i += 16) {
+    print_hex(i);
+    puts(": ");
+    for (unsigned j = 0; j < 16; j++) {
+      if (i + j < size) {
+        putchar(i2h(s[i + j] / 16));
+        putchar(i2h(s[i + j] % 16));
+        putchar(' ');
+      } else {
+        puts("   ");
+      }
+    }
+    puts("    ");
+    for (unsigned j = 0; j < 16; j++) {
+      if (i + j < size) {
+        if (0x20 <= s[i + j] && s[i + j] <= 0x7e)
+          putchar(s[i + j]);
+        else
+          putchar('.');
+      } else {
+        puts(" ");
+      }
+    }
+    puts("\n");
+  }
+  return;
+}
+
+long kernel_address = 0x80200000;
+int size = 0;
+
+void bootloader_main(int bootcore, void *dtb) {
+  puts("Hello Bootloader!\n");
+
+  print_hex((long)&bootloader_main);
+  puts("\n");
+
+  kernel_address = 0x80200000;
 
 #define BUF_SIZE 1024
   char buf[BUF_SIZE];
 
   for (;;) {
     puts("$ ");
-    int len = readline(buf, BUF_SIZE);
-    if (len <= 0)
-      continue;
-    switch (buf[0]) {
-    case 'h':
-      switch (buf[3]) {
-      case 'p':
-        puts("help\t: print this help menu\n");
-        puts("hello\t: print Hello World!\n");
-        puts("reboot\t: reboot the device\n");
-        break;
-      case 'l':
-        puts("Hello World!\n");
-        break;
-      }
+    switch (getchar()) {
+    case 'a':
+      read(&kernel_address, sizeof(kernel_address));
+      break;
+    case 'p':
+      puts("size = ");
+      print_hex(size);
+      putchar('\n');
+
+      puts("kernel_address = ");
+      print_hex(kernel_address);
+      putchar('\n');
+      break;
+    case 's':
+      read(&size, sizeof(size));
+
+      puts("size = ");
+      print_hex(size);
+      putchar('\n');
+
+      puts("kernel_address = ");
+      print_hex(kernel_address);
+      putchar('\n');
+
+      puts("start read\n");
+      read((void *)kernel_address, size);
+      puts("end read\n");
+
+      break;
+    case 'd':
+      hexdump((void *)kernel_address, size, 0);
+      break;
+    case 'j':
+      ((typeof(&bootloader_main))(kernel_address))(bootcore, dtb);
       break;
     case 'r':
       reset();
       break;
+    default:
+      puts("a\t: set kernel address\n");
+      puts("p\t: print kernel address\n");
+      puts("s\t: send kernel\n");
+      puts("d\t: dump kernel\n");
+      puts("j\t: jump to kernel\n");
+      puts("r\t: reboot the device\n");
     }
   }
 }
---
+++ upload.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import struct, sys, time
+
+split = lambda v, sz: [v[i : i + sz] for i in range(0, len(v), sz)]
+
+SERIAL = sys.argv[1]
+FILE = sys.argv[2] if len(sys.argv) > 2 else "./disk/kernel.img"
+
+with open(FILE, "rb") as f:
+    kernel = f.read()
+    size = len(kernel)
+
+with open(SERIAL, "wb", buffering=0) as tty:
+    # print("reboot")
+    # tty.write(b"reboot\n")
+    # time.sleep(3)
+
+    tty.write(b"a")
+    time.sleep(0.5)
+    tty.write(struct.pack("<Q", 0x80400000))
+    time.sleep(0.5)
+
+    tty.write(b"p")
+    time.sleep(0.5)
+
+    print("send command")
+
+    tty.write(b"s")
+    time.sleep(0.5)
+
+    print("send kernel size")
+    tty.write(struct.pack("<I", size))
+    time.sleep(0.5)
+
+    print("send kernel", len(kernel))
+    for s in split(kernel, 0x200):
+        tty.write(s)
+        time.sleep(0.01)
+
+    print("jump kernel")
+    tty.write(b"j")
