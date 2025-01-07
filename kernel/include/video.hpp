b586838f3b0a56eb8726e893d09069099cfa6304
Allen Guan <me@nella17.tw>
2025-01-07 23:44:37 +0800

    fix video

--- kernel/include/video.hpp
+++ kernel/include/video.hpp
@@ -10,4 +10,20 @@ constexpr size_t xsize = 1920;
 constexpr size_t ysize = 1080;
 constexpr size_t bpix = 5;
 
+inline volatile uint32_t& get(uint32_t x, uint32_t y) {
+  return fb[y * xsize + x];
+}
+
+inline int wait() {
+  int r = 0;
+  for (size_t i = 0; i < 4; i++)
+    for (size_t y = 0; y < ysize; y++)
+      for (size_t x = 0; x < xsize; x++)
+        if (i % 2)
+          r += get(x, y);
+        else
+          r -= get(x, y);
+  return r;
+}
+
 }  // namespace video
--- kernel/src/shell/cmd_screen_color.cpp
+++ kernel/src/shell/cmd_screen_color.cpp
@@ -7,18 +7,18 @@ using namespace video;
 int cmd_screen_color(int argc, char* argv[]) {
   if (argc < 2) {
     kprintf("%s: require color\n", argv[0]);
-    kprintf("usage: %s <color> [times]\n", argv[0]);
+    kprintf("usage: %s <color>\n", argv[0]);
     return -1;
   }
 
   uint32_t color = strtol(argv[1]);
-  size_t times = argc < 3 ? 4 : strtol(argv[2]);
-  kprintf("color = %x / times = %u\n", color, times);
+  kprintf("color = %x\n", color);
 
-  for (size_t i = 0; i < times; i++)
-    for (size_t y = 0; y < ysize; y++)
-      for (size_t x = 0; x < xsize; x++)
-        fb[y * xsize + x] = color;
+  for (size_t y = 0; y < ysize; y++)
+    for (size_t x = 0; x < xsize; x++)
+      get(x, y) = color;
+
+  wait();
 
   return 0;
 }
